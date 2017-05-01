18dff14c347515604bbbe37e455a930d13a5eae2
Terry Hayes <terrychayes@gmail.com>
2017-05-01 19:04:52 -0400

    add favicon

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="main.css">
     <link href="https://fonts.googleapis.com/css?family=Merriweather" rel="stylesheet">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
     <title>Terry Hayes</title>
   </head>
   <body>
